3398cf9a82fbc575dbf54dd0f41e241f3656cdb2
Manuel Reimer <manuel.reimer@gmx.de>
2024-01-26 17:20:54 +0100

    Version 1.0.7

--- plugin.c
+++ plugin.c
@@ -39,7 +39,7 @@
 
 
 static const char * kPluginName = "graphlcd";
-static const char *VERSION        = "1.0.6";
+static const char *VERSION        = "1.0.7";
 static const char *DESCRIPTION    = trNOOP("Output to graphic LCD");
 static const char *MAINMENUENTRY  = NULL;
 
